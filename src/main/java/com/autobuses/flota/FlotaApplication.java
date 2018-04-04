2bcc718d2781b397fc564979aedec173ec4d4067
jose luis <josemase55@misena.edu.co>
2018-04-03 23:47:59 -0500

    Primer backend

---
+++ src/main/java/com/autobuses/flota/FlotaApplication.java
@@ -0,0 +1,12 @@
+package com.autobuses.flota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlotaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/autobuses/flota/FlotaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.autobuses.flota;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlotaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
